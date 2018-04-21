ff3002df66947634bfe41a9e29c4698a67eab3d2
tilz0R <tilen.majerle@gmail.com>
2018-04-21 19:34:56 +0200

    Use DMA for transfer with more than 1 byte

--- src/system/ow_ll_stm32l4xx.c
+++ src/system/ow_ll_stm32l4xx.c
@@ -1,5 +1,5 @@
 /**
- * \file            ow_ll_stm32l4.c
+ * \file            ow_ll_stm32l4xx.c
  * \brief           UART implementation for STM32L4xx MCUs
  */
  
@@ -32,6 +32,15 @@
  */
 #include "system/ow_ll.h"
 
+/*
+ * STM32L496G-Discovery comes with external STMOD+ board with CN4 ESP-01 connector
+ *
+ * UART configuration is:
+ *
+ * UART:                USART1
+ * STM32 TX:            GPIOB, GPIO_PIN_6
+ * STM32 RX:            GPIOG, GPIO_PIN_10; Note: VDDIO2 must be enabled in PWR register
+ */
 #include "stm32l4xx_ll_usart.h"
 #include "stm32l4xx_ll_bus.h"
 #include "stm32l4xx_ll_rcc.h"
@@ -43,13 +52,16 @@
 #define ONEWIRE_USART_CLK_EN                    LL_APB2_GRP1_EnableClock(LL_APB2_GRP1_PERIPH_USART1)
 #define ONEWIRE_USART_RCC_CLOCK                 PCLK2_Frequency
 
-#define ONEWIRE_TX_PORT                         GPIOA
-#define ONEWIRE_TX_PORT_CLK_EN                  LL_AHB2_GRP1_EnableClock(LL_AHB2_GRP1_PERIPH_GPIOA)
-#define ONEWIRE_TX_PIN                          LL_GPIO_PIN_9
+#define ONEWIRE_TX_PORT                         GPIOB
+#define ONEWIRE_TX_PORT_CLK_EN                  LL_AHB2_GRP1_EnableClock(LL_AHB2_GRP1_PERIPH_GPIOB)
+#define ONEWIRE_TX_PIN                          LL_GPIO_PIN_6
 #define ONEWIRE_TX_PIN_AF                       LL_GPIO_AF_7
 
-#define ONEWIRE_RX_PORT                         GPIOA
-#define ONEWIRE_RX_PORT_CLK_EN                  LL_AHB2_GRP1_EnableClock(LL_AHB2_GRP1_PERIPH_GPIOA)
+/*
+ * Since GPIOG.10 is on VDDIO2, we have to enable VDDIO2 in power management
+ */
+#define ONEWIRE_RX_PORT                         GPIOG
+#define ONEWIRE_RX_PORT_CLK_EN                  do { LL_AHB2_GRP1_EnableClock(LL_AHB2_GRP1_PERIPH_GPIOG); LL_PWR_EnableVddIO2(); } while (0)
 #define ONEWIRE_RX_PIN                          LL_GPIO_PIN_10
 #define ONEWIRE_RX_PIN_AF                       LL_GPIO_AF_7
 
@@ -58,21 +70,21 @@
 #define ONEWIRE_USART_TX_DMA_CHANNEL            LL_DMA_CHANNEL_4
 #define ONEWIRE_USART_TX_DMA_REQUEST            LL_DMA_REQUEST_2
 #define ONEWIRE_USART_TX_DMA_CLEAR_FLAGS        do {    \
-    LL_DMA_ClearFlag_TC7(ONEWIRE_USART_TX_DMA);         \
-    LL_DMA_ClearFlag_HT7(ONEWIRE_USART_TX_DMA);         \
-    LL_DMA_ClearFlag_TE7(ONEWIRE_USART_TX_DMA);         \
+    LL_DMA_ClearFlag_TC4(ONEWIRE_USART_TX_DMA);         \
+    LL_DMA_ClearFlag_HT4(ONEWIRE_USART_TX_DMA);         \
+    LL_DMA_ClearFlag_TE4(ONEWIRE_USART_TX_DMA);         \
 } while (0)
 
-#define ONEWIRE_USART_RX_DMA                    DMA2
-#define ONEWIRE_USART_RX_DMA_CLK_EN             LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_DMA2)
-#define ONEWIRE_USART_RX_DMA_CHANNEL            LL_DMA_CHANNEL_4
+#define ONEWIRE_USART_RX_DMA                    DMA1
+#define ONEWIRE_USART_RX_DMA_CLK_EN             LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_DMA1)
+#define ONEWIRE_USART_RX_DMA_CHANNEL            LL_DMA_CHANNEL_5
 #define ONEWIRE_USART_RX_DMA_REQUEST            LL_DMA_REQUEST_2
-#define ONEWIRE_USART_RX_DMA_IRQn               DMA1_Channel4_IRQn
-#define ONEWIRE_USART_RX_DMA_IRQ_HANDLER        DMA1_Channel4_IRQHandler
+#define ONEWIRE_USART_RX_DMA_IRQn               DMA1_Channel5_IRQn
+#define ONEWIRE_USART_RX_DMA_IRQ_HANDLER        DMA1_Channel5_IRQHandler
 #define ONEWIRE_USART_RX_DMA_CLEAR_FLAGS        do {    \
-    LL_DMA_ClearFlag_TC2(ONEWIRE_USART_RX_DMA);         \
-    LL_DMA_ClearFlag_HT2(ONEWIRE_USART_RX_DMA);         \
-    LL_DMA_ClearFlag_TE2(ONEWIRE_USART_RX_DMA);         \
+    LL_DMA_ClearFlag_TC5(ONEWIRE_USART_RX_DMA);         \
+    LL_DMA_ClearFlag_HT5(ONEWIRE_USART_RX_DMA);         \
+    LL_DMA_ClearFlag_TE5(ONEWIRE_USART_RX_DMA);         \
 } while (0)
 
 /**
@@ -100,13 +112,14 @@ ow_ll_init(void* arg) {
     GPIO_InitStruct.Mode = LL_GPIO_MODE_ALTERNATE;
     GPIO_InitStruct.Speed = LL_GPIO_SPEED_FREQ_VERY_HIGH;
     GPIO_InitStruct.OutputType = LL_GPIO_OUTPUT_OPENDRAIN;
-    GPIO_InitStruct.Pull = LL_GPIO_PULL_NO;
-    GPIO_InitStruct.Alternate = LL_GPIO_AF_7;
+    GPIO_InitStruct.Pull = LL_GPIO_PULL_UP;
     
     /* TX pin */
+    GPIO_InitStruct.Alternate = ONEWIRE_TX_PIN_AF;
     GPIO_InitStruct.Pin = ONEWIRE_TX_PIN;
     LL_GPIO_Init(ONEWIRE_TX_PORT, &GPIO_InitStruct);
     /* RX pin */
+    GPIO_InitStruct.Alternate = ONEWIRE_RX_PIN_AF;
     GPIO_InitStruct.Pin = ONEWIRE_RX_PIN;
     LL_GPIO_Init(ONEWIRE_RX_PORT, &GPIO_InitStruct);
 
@@ -121,7 +134,7 @@ ow_ll_init(void* arg) {
     LL_DMA_SetPeriphAddress(ONEWIRE_USART_RX_DMA, ONEWIRE_USART_RX_DMA_CHANNEL, (uint32_t)&ONEWIRE_USART->RDR);
 
     /* USART TX DMA Init */
-    LL_DMA_SetPeriphRequest(ONEWIRE_USART_RX_DMA, ONEWIRE_USART_TX_DMA_CHANNEL, ONEWIRE_USART_TX_DMA_REQUEST);
+    LL_DMA_SetPeriphRequest(ONEWIRE_USART_TX_DMA, ONEWIRE_USART_TX_DMA_CHANNEL, ONEWIRE_USART_TX_DMA_REQUEST);
     LL_DMA_SetDataTransferDirection(ONEWIRE_USART_TX_DMA, ONEWIRE_USART_TX_DMA_CHANNEL, LL_DMA_DIRECTION_MEMORY_TO_PERIPH);
     LL_DMA_SetMode(ONEWIRE_USART_TX_DMA, ONEWIRE_USART_TX_DMA_CHANNEL, LL_DMA_MODE_NORMAL);
     LL_DMA_SetPeriphIncMode(ONEWIRE_USART_TX_DMA, ONEWIRE_USART_TX_DMA_CHANNEL, LL_DMA_PERIPH_NOINCREMENT);
@@ -131,8 +144,8 @@ ow_ll_init(void* arg) {
     LL_DMA_SetPeriphAddress(ONEWIRE_USART_TX_DMA, ONEWIRE_USART_TX_DMA_CHANNEL, (uint32_t)&ONEWIRE_USART->TDR);
     
     /* Enable DMA RX interrupt */
-    NVIC_SetPriority(ONEWIRE_USART_RX_DMA_IRQn, NVIC_EncodePriority(NVIC_GetPriorityGrouping(), 1, 0));
-    NVIC_EnableIRQ(ONEWIRE_USART_RX_DMA_IRQn);
+//    NVIC_SetPriority(ONEWIRE_USART_RX_DMA_IRQn, NVIC_EncodePriority(NVIC_GetPriorityGrouping(), 1, 0));
+//    NVIC_EnableIRQ(ONEWIRE_USART_RX_DMA_IRQn);
 
     /* Configure UART peripherals */
     USART_InitStruct.BaudRate = 9600;
@@ -187,25 +200,35 @@ ow_ll_set_baudrate(uint32_t baud, void* arg) {
  */
 uint8_t
 ow_ll_transmit_receive(const void* tx, void* rx, size_t len, void* arg) {
+    const uint8_t* t = tx;
+    uint8_t* r = rx;
+    
+    if (len == 1) {
+        LL_USART_TransmitData8(ONEWIRE_USART, *t);
+        while (!LL_USART_IsActiveFlag_TXE(ONEWIRE_USART));
+        while (!LL_USART_IsActiveFlag_RXNE(ONEWIRE_USART));
+        *r = LL_USART_ReceiveData8(ONEWIRE_USART);
+        return 1;
+    }
+    
     /* Clear all DMA flags */
+    LL_USART_ClearFlag_TC(ONEWIRE_USART);
     ONEWIRE_USART_RX_DMA_CLEAR_FLAGS;
     ONEWIRE_USART_TX_DMA_CLEAR_FLAGS;
     
     /* Set data length */
     LL_DMA_SetDataLength(ONEWIRE_USART_RX_DMA, ONEWIRE_USART_RX_DMA_CHANNEL, len);
     LL_DMA_SetDataLength(ONEWIRE_USART_TX_DMA, ONEWIRE_USART_TX_DMA_CHANNEL, len);
-    /* Set memory addresses */
-    LL_DMA_SetMemoryAddress(ONEWIRE_USART_RX_DMA, ONEWIRE_USART_RX_DMA_CHANNEL, (uint32_t)rx);
-    LL_DMA_SetMemoryAddress(ONEWIRE_USART_TX_DMA, ONEWIRE_USART_TX_DMA_CHANNEL, (uint32_t)tx);
     
-    /* Enable UART DMA requests and start stream */
+    /* Set memory addresses */
+    LL_DMA_SetMemoryAddress(ONEWIRE_USART_RX_DMA, ONEWIRE_USART_RX_DMA_CHANNEL, (uint32_t)r);
+    LL_DMA_SetMemoryAddress(ONEWIRE_USART_TX_DMA, ONEWIRE_USART_TX_DMA_CHANNEL, (uint32_t)t);
+    
+    /* Enable TX and RX requests */
     LL_USART_EnableDMAReq_RX(ONEWIRE_USART);
     LL_USART_EnableDMAReq_TX(ONEWIRE_USART);
     
-    /* Enable transfer complete interrupt for RX */
-    LL_DMA_EnableIT_TC(ONEWIRE_USART_RX_DMA, ONEWIRE_USART_RX_DMA_CHANNEL);
-    
-    /* Enable streams and start transfer */
+    /* Enable UART DMA requests and start stream */
     LL_DMA_EnableChannel(ONEWIRE_USART_RX_DMA, ONEWIRE_USART_RX_DMA_CHANNEL);
     LL_DMA_EnableChannel(ONEWIRE_USART_TX_DMA, ONEWIRE_USART_TX_DMA_CHANNEL);
     
@@ -218,6 +241,8 @@ ow_ll_transmit_receive(const void* tx, void* rx, size_t len, void* arg) {
     while (LL_DMA_GetDataLength(ONEWIRE_USART_RX_DMA, ONEWIRE_USART_RX_DMA_CHANNEL));
     
     /* Disable requests */
+    LL_DMA_DisableChannel(ONEWIRE_USART_RX_DMA, ONEWIRE_USART_RX_DMA_CHANNEL);
+    LL_DMA_DisableChannel(ONEWIRE_USART_TX_DMA, ONEWIRE_USART_TX_DMA_CHANNEL);
     LL_USART_DisableDMAReq_RX(ONEWIRE_USART);
     LL_USART_DisableDMAReq_TX(ONEWIRE_USART);
     
