1d1fdfb3cea3caaf4aaedb4bb0f927d02eaf7d9e
tilz0R <tilen.majerle@gmail.com>
2018-06-30 14:50:41 +0200

    Add example for STm32L496 Discovery

---
+++ examples/stm32l496g-discovery/ow_ds18b20_RTOS/inc/ow_config.h
@@ -0,0 +1,47 @@
+/**
+ * \file            ow_config.h
+ * \brief           Configuration for OW
+ */
+
+/*
+ * Copyright (c) 2018 Tilen Majerle
+ *  
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge,
+ * publish, distribute, sublicense, and/or sell copies of the Software, 
+ * and to permit persons to whom the Software is furnished to do so, 
+ * subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE
+ * AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * This file is part of OneWire-UART.
+ *
+ * Author:          Tilen MAJERLE <tilen@majerle.eu>
+ */
+#ifndef __OW_CONFIG_H
+#define __OW_CONFIG_H
+
+#include "cmsis_os.h"
+
+/*
+ * User specific config which overwrites setup from ow_config_default.h file
+ */
+#define OW_CFG_OS                               1
+#define OW_CFG_OS_MUTEX_HANDLE                  osMutexId
+
+/* Include default configuration setup */
+#include "ow/ow_config_default.h"
+
+#endif /* __OW_CONFIG_H */
