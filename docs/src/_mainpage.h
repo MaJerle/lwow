133575bf9c5e82e66aa471f5271304f329fb9876
Tilen Majerle <tilen.majerle@gmail.com>
2019-11-02 13:02:36 +0100

    Remove wrong link to repository resources

--- docs/src/_mainpage.h
+++ docs/src/_mainpage.h
@@ -14,8 +14,8 @@
  *
  * \section         sect_resources Download & Resources
  *
- *  - <a class="download_url" href="https://github.com/MaJerle/onewire_uart/releases">Download library from Github releases</a>
- *  - <a href="https://github.com/MaJerle/onewire_uart_res">Resources and examples repository</a>
+ *  - <a class="download_url" href="https://github.com/MaJerle/onewire_uart/releases">Download library at Github releases</a>
+ *  - <a href="https://github.com/MaJerle/onewire_uart">Resources and examples repository</a>
  *  - Read \ref page_appnote before you start development
  *  - <a href="https://github.com/MaJerle/onewire_uart">Official development repository on Github</a>
  *  - <a href="https://www.maximintegrated.com/en/app-notes/index.mvp/id/214">Using a UART to Implement a 1-Wire Bus Master</a> from Maxim website
