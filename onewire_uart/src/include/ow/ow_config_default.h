cfe31d0789a16b852ece0dfab5b3c0efc48b32aa
Tilen Majerle <tilen.majerle@gmail.com>
2019-12-08 00:40:59 +0100

    Set version 2.0.0

--- onewire_uart/src/include/ow/ow_config_default.h
+++ onewire_uart/src/include/ow/ow_config_default.h
@@ -29,7 +29,7 @@
  * This file is part of OneWire-UART library.
  *
  * Author:          Tilen MAJERLE <tilen@majerle.eu>
- * Version:         v1.2.0
+ * Version:         v2.0.0
  */
 #ifndef OW_HDR_CONFIG_DEFAULT_H
 #define OW_HDR_CONFIG_DEFAULT_H
