740b782c01c264c6deffe03fa8b7c9cabd127531
Tilen Majerle <tilen.majerle@gmail.com>
2020-11-15 11:25:01 +0100

    Set version to 3.0.1

--- lwow/src/system/lwow_sys_cmsis_os.c
+++ lwow/src/system/lwow_sys_cmsis_os.c
@@ -29,7 +29,7 @@
  * This file is part of LwOW - Lightweight onewire library.
  *
  * Author:          Tilen MAJERLE <tilen@majerle.eu>
- * Version:         v3.0.0
+ * Version:         v3.0.1
  */
 #include "lwow/lwow.h"
 
