039c3c914b3d981122b1ef49d9ba4ab47afdc5c7
Tilen Majerle <tilen.majerle@gmail.com>
2020-12-05 08:58:16 +0100

    Add v prefix in version

--- docs/conf.py
+++ docs/conf.py
@@ -27,7 +27,7 @@ copyright = '2020, Tilen MAJERLE'
 author = 'Tilen MAJERLE'
 
 # The full version, including alpha/beta/rc tags
-version = '3.0.1'
+version = 'v3.0.1'
 
 # Try to get branch at which this is running
 # and try to determine which version to display in sphinx
